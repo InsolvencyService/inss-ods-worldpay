3c374c9c67f38b9a54b0f8cc75068a18aede83ae
Francis Tchatchoua Njiki <tchatchoua@gmail.com>
2022-09-12 16:48:13 +0100

    ANC-46 Removed legacy WorldPay MVC project

--- Insolvency.CommonServices.WorldpayProxy/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Insolvency.CommonServices.WorldpayProxy.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
